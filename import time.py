4d2ffbf2860f8c8483751003e8afb84a32f2152c
dylangalaura <87359856+dylangalaura@users.noreply.github.com>
2021-07-14 23:41:42 -0700

    Create import time.py

---
+++ import time.py
@@ -0,0 +1,11 @@
+import time
+import webbrowser
+
+def elton_john():
+
+	x = 0
+
+	while x == 0:
+		#webbrowser.open(r"F:\Wating Folder\Streamdeck\Streamdeck 2021.07.11 - 21.10.34.07.mp4")
+		time.sleep(604800)
+elton_john()
